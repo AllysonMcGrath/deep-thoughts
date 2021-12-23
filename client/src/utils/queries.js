349ec9d11fe3230ef534c6e3b7e3b1aaf73c2768
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2021-12-23 13:44:08 -0600

    add client components, add QUERY_THOUGHTS

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+//this file has all graphql query requests
+import { gql } from '@apollo/client';
+
+export const QUERY_THOUGHTS = gql`
+    query thoughts($username: String) {
+        thoughts(username: $username) {
+            _id
+            thoughtText
+            createdAt
+            username
+            reactionCount
+            reactions {
+                _id
+                createdAt
+                username
+                reactionBody
+            }
+        }
+    }
+`;
